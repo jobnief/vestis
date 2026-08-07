58de1b6ec8f60738e930b9b8940b08e5595055c4
Jobnief <tobiasparedes497@gmail.com>
2026-08-07 20:32:09 -0300

    Add optional garment brand

--- src/vestis/models/garment.py
+++ src/vestis/models/garment.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
 from vestis.enums.category import Category 
+from typing import Optional
 
 @dataclass
 class Garment:
+    name: str 
     category: Category
+    brand: Optional[str] = None 
+
 
 
